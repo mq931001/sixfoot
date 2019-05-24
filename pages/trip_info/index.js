ca79e271a709ba6931382efe2e95050c8504898c
mq931001 <872693931@qq.com>
2019-05-24 16:16:02 +0800

    轨迹线路修改

--- pages/trip_info/index.js
+++ pages/trip_info/index.js
@@ -5,51 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
-    tripId:'',
+    tripId: '',
     tripObj: {
       page_num: 0,
       page_size: 40
-    }
+    },
+    polyline: [{
+      points: [],
+      color: '#FF0000DD',
+      width: 4,
+      borderWidth: 10,
+    }],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     console.log(options)
     this.data.tripId = options.id
-    this.getImgAjax()
+    this.getTripAjax()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
-  // 获取数据
-  getImgAjax:function() {
+  // 获取线路数据
+  getImgAjax: function() {
     var _this = this
     wx.request({
       url: 'https://www.foooooot.com/api/v3/trip/66/?richtxt=1/',
       data: _this.data.tripObj,
-      success: function (res) { console.log(res) },
-      fail: function (res) { },
-      complete: function (res) { },
+      success: function(res) {
+        console.log(res)
+      },
+      fail: function(res) {},
+      complete: function(res) {},
     })
   },
-  //获取数据详情
-  getTripAjax: function () {
+  //获取线路经纬度
+  getTripAjax: function() {
     var _this = this
     wx.request({
-      url: 'https://www.foooooot.com/api/v3/trip/' + _this.data.tripId + '?richtxt=1',
-      header: {},
+      url: 'https://www.foooooot.com/trip/66/offsettrackjson/',
       method: 'GET',
-      dataType: 'json',
-      responseType: 'text',
-      success: function (res) { console.log(res) },
-      fail: function (res) { },
-      complete: function (res) { },
+      success: function(res) {
+        console.log(res)
+        var data = res.data
+        for (var i = 0; i < data.length; i++) {
+          _this.data.polyline[0].points.push({
+            longitude: data[i][2],
+            latitude: data[i][1]
+          })
+        }
+        _this.setData({
+          'polyline[0].points': _this.data.polyline[0].points
+        })
+        var map = wx.createMapContext('map');
+        map.includePoints({
+          points: _this.data.polyline[0].points,
+          success: function(res) {
+            console.log(res)
+          },
+          fail: function(err) {
+            console.log(err)
+          }
+        })
+      },
+      fail: function(res) {},
+      complete: function(res) {},
     })
   }
 
